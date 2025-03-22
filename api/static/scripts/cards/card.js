d98dcd0cee74532509bc9fd372543de0a6c29d18
WebShining <webshining@protonmail.com>
2025-03-22 20:30:30 +0200

    fixed mobile cards for ios devices

--- api/static/scripts/cards/card.js
+++ api/static/scripts/cards/card.js
@@ -1,85 +1,76 @@
-let isDragging = false;
+const card = () => document.querySelector(".card");
+const cardFront = () => document.querySelector(".card__front");
+const cardBack = () => document.querySelector(".card__back");
+const variantNo = () => document.querySelector(".variants__no");
+const variantYes = () => document.querySelector(".variants__yes");
 let isMove = false;
-let startX = 0;
+let isDrag = false;
 let isActive = false;
+let lastOffsetX = 0;
+let startX = 0;
 
-const onDragStart = (e, card, variantNo, variantYes) => {
+const onDragStart = (e) => {
 	if (isActive) return;
-	isDragging = true;
-	startX = e.touches ? e.touches[0].clientX : e.clientX;
+	startX = getOffsetX(e);
+	isDrag = true;
 
-	toggleDraggingClass(card, variantNo, variantYes);
+	card().style.transition = "none";
+	variantNo().style.transition = "none";
+	variantYes().style.transition = "none";
 };
 
-const onDragMove = (e, card, variantNo, variantYes) => {
-	if (!isDragging || isActive) return;
-
+const onDragMove = (e) => {
+	if (!isDrag || isActive) return;
 	const offsetX = getOffsetX(e);
-
 	if (Math.abs(offsetX) > 5 || isMove) {
 		if (!isMove) isMove = true;
-		card.style.transform = `translateX(${offsetX}px) rotate(${offsetX / 15}deg)`;
+		card().style.transform = `translateX(${offsetX}px) rotate(${offsetX / 15}deg)`;
+		lastOffsetX = offsetX;
+		if (offsetX > 0) {
+			variantNo().style.opacity = 1;
+			variantNo().style.transform = `translateX(${Math.max(-offsetX, -125)}px)`;
+			variantYes().style.opacity = "";
+			variantYes().style.transform = "";
+		} else if (offsetX < 0) {
+			variantNo().style.opacity = "";
+			variantNo().style.transform = "";
+			variantYes().style.opacity = 1;
+			variantYes().style.transform = `translateX(${Math.min(-offsetX, 125)}px)`;
+		} else {
+			variantNo().style.opacity = "";
+			variantNo().style.transform = "";
+			variantYes().style.opacity = "";
+			variantYes().style.transform = "";
+		}
 	}
-	updateVariantStyles(variantNo, variantYes, undefined, undefined, offsetX);
 };
 
-const onDragEnd = (e, card, cardFront, cardBack, variantNo, variantYes) => {
-	if (!isDragging || isActive) return;
-	isDragging = false;
-	toggleDraggingClass(card, variantNo, variantYes);
-	card.style.transform = "none";
-	updateVariantStyles(variantNo, variantYes);
-	const offsetX = getOffsetX(e);
-	if (offsetX >= 100) onChange(card, cardFront, cardBack, false);
-	else if (offsetX <= -100) onChange(card, cardFront, cardBack, true);
+const onDragEnd = (e) => {
+	card().style.transition = "";
+	card().style.transform = "";
+	variantNo().style.opacity = "";
+	variantNo().style.transform = "";
+	variantNo().style.transition = "";
+	variantYes().style.opacity = "";
+	variantYes().style.transform = "";
+	variantYes().style.transition = "";
+
+	processCard();
+
+	lastOffsetX = 0;
+	startX = 0;
+	isDrag = false;
 	setTimeout(() => {
 		isMove = false;
-	}, 200);
+	}, 300);
 };
 
-const onClick = (card) => {
+const onClick = () => {
 	if (isMove) return;
-	card.classList.toggle("active");
-	isActive = card.classList.contains("active");
+	isActive = !isActive;
+	card().classList.toggle("active");
 };
 
-const onChange = (card, cardFront, cardBack, know = null) => {
-	const word_id = card.getAttribute("data-id");
-	if (word_id) {
-		auth_axios.put(`/words/${dictionary.id}/${word_id}`, { know });
-		window.dictionary.words = window.dictionary.words.filter((word) => word.id != word_id);
-	}
-	if (window.dictionary.words.length > 0) {
-		card.setAttribute("data-id", window.dictionary.words[0].id);
-		cardFront.innerHTML = `${window.dictionary.words[0].word}`;
-		cardBack.innerHTML = `<span>${window.dictionary.words[0].translate}</span>`;
-	} else {
-		card.removeAttribute("data-id");
-		cardFront.innerHTML = `That's all`;
-		cardBack.innerHTML = `<span>На этом все</span>`;
-	}
-};
-
-const updateVariantStyles = (variantNo, variantYes, param = undefined, value = undefined, offsetX = 0) => {
-	if (param && value) {
-		variantNo.style["param"] = value;
-		variantYes.style["param"] = value;
-	} else if (offsetX > 5) {
-		variantNo.style.opacity = "1";
-		variantYes.style.opacity = "0";
-		variantNo.style.transform = `translateX(${-Math.min(offsetX, 125)}px)`;
-	} else if (offsetX < -5) {
-		variantNo.style.opacity = "0";
-		variantYes.style.opacity = "1";
-		variantYes.style.transform = `translateX(${-Math.max(offsetX, -125)}px)`;
-	} else {
-		variantNo.style.opacity = "0";
-		variantYes.style.opacity = "0";
-
-		variantNo.style.transform = "none";
-		variantYes.style.transform = "none";
-	}
-};
 const getOffsetX = (e) => {
 	if (e.touches && e.touches.length > 0) {
 		return e.touches[0].clientX - startX;
@@ -89,8 +80,28 @@ const getOffsetX = (e) => {
 		return e.clientX - startX;
 	}
 };
-const toggleDraggingClass = (...items) => {
-	for (i of items) {
-		i.classList.toggle("dragging_on");
+
+const processCard = () => {
+	let know = undefined;
+	if (lastOffsetX < -100) {
+		know = true;
+	} else if (lastOffsetX > 100) {
+		know = false;
+	}
+
+	const word_id = card().getAttribute("data-id");
+	if (word_id && know !== undefined) {
+		console.log("A");
+		auth_axios.put(`/words/${window.dictionary.id}/${card().getAttribute("data-id")}`, { know });
+		window.dictionary.words = window.dictionary.words.filter((word) => word.id !== Number(word_id));
+	}
+	if (window.dictionary.words.length > 0) {
+		card().setAttribute("data-id", window.dictionary.words[0].id);
+		cardFront().innerHTML = `${window.dictionary.words[0].word}`;
+		cardBack().innerHTML = `<span>${window.dictionary.words[0].translate}</span>`;
+	} else {
+		card().removeAttribute("data-id");
+		cardFront().innerHTML = `That's all`;
+		cardBack().innerHTML = `<span>На этом все</span>`;
 	}
 };
--- api/static/scripts/cards/scripts.js
+++ api/static/scripts/cards/scripts.js
@@ -1,18 +1,14 @@
 document.addEventListener("DOMContentLoaded", async () => {
 	const card = document.querySelector(".card");
-	const cardFront = document.querySelector(".card__front");
-	const cardBack = document.querySelector(".card__back");
-	const variantNo = document.querySelector(".variants__no");
-	const variantYes = document.querySelector(".variants__yes");
-	onChange(card, cardFront, cardBack);
+	processCard();
 
-	card.addEventListener("mousedown", (e) => onDragStart(e, card, variantNo, variantYes));
-	document.addEventListener("mousemove", (e) => onDragMove(e, card, variantNo, variantYes));
-	document.addEventListener("mouseup", (e) => onDragEnd(e, card, cardFront, cardBack, variantNo, variantYes));
+	card.addEventListener("mousedown", (e) => onDragStart(e));
+	document.addEventListener("mousemove", (e) => onDragMove(e));
+	document.addEventListener("mouseup", (e) => onDragEnd(e));
 
 	card.addEventListener("touchstart", (e) => onDragStart(e, card, variantNo, variantYes));
 	document.addEventListener("touchmove", (e) => onDragMove(e, card, variantNo, variantYes));
 	document.addEventListener("touchend", (e) => onDragEnd(e, card, cardFront, cardBack, variantNo, variantYes));
 
-	card.addEventListener("click", () => onClick(card));
+	card.addEventListener("click", () => onClick());
 });
