5665ccd099754e04b2dbade50af62cb3018a0345
webshining <webshining@protonmail.com>
2026-02-06 03:10:12 +0200

    added delete router

--- api/routes/words.py
+++ api/routes/words.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
-# from sqlalchemy.ext.asyncio import AsyncSession
 from api.depends import current_user
 from api.models.words import Response as WordResponse
 from database import get_session_generator
-from database.models import User
+from database.models import User, Word
 
 router = APIRouter(prefix="/words", dependencies=[Depends(get_session_generator)])
 
@@ -13,3 +13,11 @@ router = APIRouter(prefix="/words", dependencies=[Depends(get_session_generator)
 async def _words(current_user: User = Depends(current_user)):
     await current_user.awaitable_attrs.words
     return [w.to_dict() for w in current_user.words]
+
+
+@router.delete("/{id}")
+async def _word_delete(id: int, current_user: User = Depends(current_user), session: AsyncSession = Depends(get_session_generator)):
+    word = await Word.get_by(Word.user_id == current_user.id, Word.id == id, session=session)
+    if word:
+        await session.delete(word)
+    return "ok"
